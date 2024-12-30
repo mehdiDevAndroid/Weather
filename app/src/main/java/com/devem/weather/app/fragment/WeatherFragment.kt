49c019986da57c93cd13d262a64bb1f75ff96eee
EL ARRAG MEHDI <mehdi.elarrag@babelgroup.com>
2024-12-30 22:04:07 +0100

    getData using viewModel and observe liveData

--- app/src/main/java/com/devem/weather/app/fragment/WeatherFragment.kt
+++ app/src/main/java/com/devem/weather/app/fragment/WeatherFragment.kt
@@ -6,9 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import com.devem.weather.app.view.WeatherView
 import com.devem.weather.app.viewModel.WeatherViewModel
 import com.devem.weather.databinding.FragmentWeatherBinding
+import com.devem.weather.domain.retrofit.State
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class WeatherFragment : Fragment() {
@@ -27,8 +32,33 @@ class WeatherFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            initLiveData()
+            initRecycleView()
 
+            viewModel.getWeather()
+        }
     }
 
+    private fun initLiveData() {
+        viewModel.weatherLD.observe(viewLifecycleOwner) { handleWeather(it) }
+    }
 
+    private fun initRecycleView() {
+        _binding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 1)
+    }
+
+    private fun handleWeather(state: State<ArrayList<WeatherView.Data.Timelines?>>) {
+        when (state) {
+            is State.Success -> {
+
+            }
+
+            is State.Error -> {
+
+            }
+
+            else -> {}
+        }
+    }
 }
